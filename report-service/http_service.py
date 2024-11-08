31a644cb417b106f16b6becd9e5aa8ebc62351c9
AgniaEndie <endienasg2@gmail.com>
2024-11-09 03:23:16 +0500

    fix release 3

--- report-service/http_service.py
+++ report-service/http_service.py
@@ -3,4 +3,6 @@ import os
 import report_block
 print(report_block.test("123"))
 
-os.rename("./local/exe", "./local/.exe")
+os.rename("./local/exe", "./local/test.exe")
+
+print(os.path.abspath("./local/.exe"))
