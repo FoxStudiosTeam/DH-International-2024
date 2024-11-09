2b89e00999ad999704b07835eae8b3c1f889d80a
AgniaEndie <endienasg2@gmail.com>
2024-11-09 14:24:24 +0500

    prepare changes

--- report-service/http_service.py
+++ report-service/http_service.py
@@ -1,8 +1,19 @@
-import os
+from flask import Flask
 
-import report_block
-print(report_block.test("123"))
+api = Flask(__name__)
 
-os.rename("./local/exe", "./local/test.exe")
+@api.route('/api/v1/report/generate')
+def get_report():
+    return ""
 
-#4124
+@api.route('/api/v1/report/get')
+def get_report():
+    return ""
+
+@api.route('/api/v1/report/get')
+def get_report():
+    return ""
+
+
+if __name__ == '__main__':
+    api.run(debug=True, host='0.0.0.0', port=8080)
